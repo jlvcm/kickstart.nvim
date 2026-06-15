41b68aaec5d5a95295088e05b30a6979ac954e79
João Melo <joaolvcm@gmail.com>
2026-06-15 10:34:34 +0100

    add tmux navigation

--- init.lua
+++ init.lua
@@ -223,10 +223,8 @@ do
   --  Use CTRL+<hjkl> to switch between windows
   --
   --  See `:help wincmd` for a list of all window commands
-  vim.keymap.set('n', '<C-h>', '<C-w><C-h>', { desc = 'Move focus to the left window' })
-  vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right window' })
-  vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
-  vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
+  -- <C-h/j/k/l> are handled by vim-tmux-navigator (lua/custom/plugins/tmux-navigator.lua)
+  -- which seamlessly crosses into tmux panes at window edges.
 
   -- NOTE: Some terminals have colliding keymaps or are not able to send distinct keycodes
   -- vim.keymap.set("n", "<C-S-h>", "<C-w>H", { desc = "Move window to the left" })
